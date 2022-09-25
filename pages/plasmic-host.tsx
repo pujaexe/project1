6f55700effd63324e1019351a37534bfaf671ddb
pujaexe <puja.exe@gmail.com>
2022-09-25 23:03:19 +0800

    update

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -4,6 +4,8 @@ import Script from 'next/script';
 import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
 import { PLASMIC } from '../plasmic-init';
 
+
+
 export default function PlasmicHost() {
   return PLASMIC && <PlasmicCanvasHost />;
 }
